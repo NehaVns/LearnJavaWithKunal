8e2688f62444febb75cf1ea8258e0d0332435fec
NehaVns <nehavns2848@gmail.com>
2023-02-26 12:32:09 +0530

    Day 3

--- src/com/gitquestionpractice/firstjava/FibonacciSeries.java
+++ src/com/gitquestionpractice/firstjava/FibonacciSeries.java
@@ -1,5 +1,14 @@
 package com.gitquestionpractice.firstjava;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of terms in th series: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+
+    }
 
 }
